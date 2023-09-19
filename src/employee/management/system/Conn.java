37937ad291f99f78e5405b59bb0d9f4eab10be47
vikaskr-gupta <vvikaskgupta86@gmail.com>
2023-09-19 23:37:29 +0530

    Add initial files

---
+++ src/employee/management/system/Conn.java
@@ -0,0 +1,19 @@
+package employee.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagementsystem", "root", "Vikas@76");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
